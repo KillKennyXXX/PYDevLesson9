c8add642d5b4179cd732461a90af6e80be8fe0ab
Alexey Shalygin <84471084+KillKennyXXX@users.noreply.github.com>
2022-10-20 18:01:51 +0300

    1

--- game_loto.py
+++ game_loto.py
@@ -4,104 +4,132 @@ from random import randint
 class Card:
     def __init__(self):
         self.name = 'Компьютер'  # Имя игрока
-        self.barrels = set()  # Множество боченков игрока
-        self.cardList = [[0 for j in range(9)] for i in range(3)]  # Карточка игрока
-        [self.barrels.add(randint(1, 90)) for i in range(1000) if len(self.barrels) < 15]
-        self.barrels = list(self.barrels)
-        self.barrels.sort()
+        self._barrels = set()  # Множество боченков игрока
+        self._cardList = [[0 for j in range(9)] for i in range(3)]  # Карточка игрока
+        [self._barrels.add(randint(1, 90)) for i in range(1000) if len(self._barrels) < 15]
+        self._barrels = list(self._barrels)
+        self._barrels.sort()
         count = 0
         for i in range(3):
             nums = 0
             while nums < 5:
                 y = randint(0, 8)
-                if self.cardList[i][y] == 0:
-                    self.cardList[i][y] = self.barrels[count]
+                if self._cardList[i][y] == 0:
+                    self._cardList[i][y] = self._barrels[count]
                     nums += 1
                     count += 1
+        # print(self._cardList)
 
-    def view(self):
-        print('*' * int(15 - len(self.name) / 2) + ' ' + self.name + ' ' + '*' * int(15 - len(self.name) / 2))
+    @property
+    def barrels(self):
+        return self._barrels
+
+    @property
+    def cardList(self):
+        return self._cardList
+
+    def __str__(self):
+        strA = '*' * int(15 - len(self.name) / 2) + ' ' + self.name + ' ' + '*' * int(15 - len(self.name) / 2)
+        strC = '*' * 31 + '\n'
+        strB = ''
         for i in range(3):
-            strA = ''
+            strB += '\n'
             for j in range(9):
-                strA += f' {self.cardList[i][j]} ' if self.cardList[i][j] else '   '
-            print(strA)
-        print('*' * 31)
+                strB += f' {self._cardList[i][j]} ' if self._cardList[i][j] else '   '
+        strB += '\n'
 
-    def get_name(self):
+        return strA + strB + strC
+
+    def set_name(self):
         f = input('Как к вам обращаться? ')
         self.name = f if len(f) < 30 and len(f) else 'Игрок'
 
     def dell_card(self, barrel):
-        self.barrels.remove(barrel)
+        self._barrels.remove(barrel)
         for i in range(3):
             for j in range(9):
-                if self.cardList[i][j] == barrel:
-                    self.cardList[i][j] = 0
+                if self._cardList[i][j] == barrel:
+                    self._cardList[i][j] = 0
                     break
 
     def check_card(self, barrel):
-        if barrel in self.barrels:
+        if barrel in self._barrels:
             self.dell_card(barrel)
-            if len(self.barrels) >= 1:
+            if len(self) >= 1:
                 return 1
             else:
                 return 2
         else:
             return 0
 
+    def __len__(self):
+        return len(self._barrels)
+
 
 class Bag:
     def __init__(self):
-        self.barrels = set()
+        self._barrels = set()
+        self._barrel = 0
+
+    @property
+    def barrels(self):
+        return self._barrels
+
+    @property
+    def barrel(self):
+        return self._barrel
 
     def new_barrel(self):
-        nums = len(self.barrels)
-        while nums == len(self.barrels):
+        nums = len(self)
+        while nums == len(self):
             barrel = randint(1, 90)
-            self.barrels.add(barrel)
-        return barrel
+            self._barrels.add(barrel)
+        self._barrel = barrel
 
-    def view(self, barrel):
-        return f'Новый боченок {barrel}, осталось {90 - len(self.barrels)} бочонков.'
+    def __str__(self):
+        return f'\nНовый бочонок {self._barrel}, осталось {90 - len(self)} бочонков.'
+
+    def __len__(self):
+        return len(self._barrels)
+
 
 
 def game():
     card_user = Card()
-    card_user.get_name()
+    card_user.set_name()
     card_comp = Card()
     bag_new = Bag()
     print('Начинаем игру.')
 
     while True:
-        barrel = bag_new.new_barrel()
-        print(bag_new.view(barrel))
+        bag_new.new_barrel()
+        print(bag_new)
         print('Ваши карточки:')
-        card_user.view()
-        card_comp.view()
-        user_choice = input('Вычеркиваем боченок? (Да): ').upper()
+        print(card_user)
+        print(card_comp)
+        user_choice = input('Вычеркиваем бочонок? (Да): ').upper()
         if user_choice == 'ДА':
-            check = card_user.check_card(barrel)
+            check = card_user.check_card(bag_new.barrel)
             if check == 0:
                 print('Вы проиграли!')
                 break
             elif check == 1:
-                print(f'{card_user.name} зачеркнул боченок {barrel}\n')
+                print(f'{card_user.name} зачеркнул бочонок {bag_new.barrel}')
             elif check == 2:
                 print('Вы выиграли!')
                 break
-        elif barrel in card_user.barrels:
+        elif bag_new.barrel in card_user.barrels:
             print('Вы проиграли!')
             break
 
-
-        if barrel in card_comp.barrels:
-            check = card_comp.check_card(barrel)
-            print(f'Компьютер зачеркнул боченок {barrel}')
+        if bag_new.barrel in card_comp.barrels:
+            check = card_comp.check_card(bag_new.barrel)
+            print(f'Компьютер зачеркнул бочонок {bag_new.barrel}')
             if check == 2:
                 print('Компьютер выиграл!')
                 break
-        print()
+
+
 
 if __name__ == '__main__':
     game()
---
+++ test_game_loto.py
@@ -0,0 +1,49 @@
+import unittest
+import game_loto as gl
+
+class Test_Card(unittest.TestCase):
+    def setUp(self):
+        self.card = gl.Card()
+
+    def tearDown(self):
+        pass
+
+    def test_card(self):
+        self.assertEqual(len(self.card),  15)
+        self.assertEqual(len(self.card.barrels), 15)
+        for barrel in self.card.barrels:
+            self.assertTrue(barrel in range(90))
+        self.assertEqual(len(self.card.cardList), 3)
+        self.assertEqual(len(self.card.cardList[0]), 9)
+        barrel = self.card.barrels[0]
+        self.assertTrue(barrel in self.card.barrels)
+        self.card.dell_card(barrel)
+        self.assertEqual(len(self.card.barrels), 14)
+        self.assertFalse(barrel in self.card.barrels)
+        count = 14
+        for barrel in self.card.barrels:
+            count -= 1
+            self.assertTrue(barrel in self.card.barrels)
+            self.assertEqual(self.card.check_card(barrel), 1) if count > 0 else self.assertEqual(self.card.check_card(barrel), 2)
+            self.assertEqual(self.card.check_card(barrel), 0)
+            self.assertFalse(barrel in self.card.barrels)
+
+class Test_Bag(unittest.TestCase):
+    def setUp(self):
+        self.bag = gl.Bag()
+
+    def tearDown(self):
+        pass
+
+    def test_bag(self):
+        self.assertEqual(len(self.bag),  0)
+        self.assertEqual(self.bag.barrel, 0)
+        self.assertEqual(len(self.bag.barrels), 0)
+        self.assertEqual(self.bag.barrels, set())
+        self.bag.new_barrel()
+        self.assertEqual(len(self.bag), 1)
+        self.assertTrue(self.bag.barrel in range(90))
+        self.assertTrue(self.bag.barrel != 0)
+        self.assertEqual(len(self.bag.barrels), 1)
+
+
